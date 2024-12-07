d43f3545d2f18fcebccd7b56c4255f480eba884c
Adithyenkandasamy <adithyen1@gmail.com>
2024-12-07 23:48:08 +0530

    i think the error will come so try except i use

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ output = "output1.png"
 with open(input_image,"rb") as input_file:
     in_image = input_file.read()
 
+with open(output_image,"wb") as output_file:
+     output_file.write(out_image) 
+
 
 out_image = remove(in_image)
