6e50531b7d95dbeb1cbbd33e7397696b3a3047d4
Adithyenkandasamy <adithyen1@gmail.com>
2024-12-08 00:11:39 +0530

    ds

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from rembg import remove
 from PIL import Image
 import io
 
-input_image = "input1.png"  # Original AVIF file
+input_image = "input1.avif"  # Original AVIF file
 intermediate_image = "converted_input1.png"  # Temporary PNG file
 output_image = "output1.png"  # Final file after background removal
 
